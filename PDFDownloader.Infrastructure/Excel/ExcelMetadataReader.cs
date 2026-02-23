35f2532a5a40953c1f96f954dfe677a32cc35c5d
ThomasBlichfeldt2409 <Thomas.Blichfeldt@specialisterne.dk>
2026-02-23 13:39:17 +0100

    Added structure for Infrastructure and Tests

---
+++ PDFDownloader.Infrastructure/Excel/ExcelMetadataReader.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFDownloader.Infrastructure.Excel
+{
+    internal class ExcelMetadataReader
+    {
+    }
+}
---
+++ PDFDownloader.Tests/Services/ReportDownloadServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDFDownloader.Tests.Services
+{
+    internal class ReportDownloadServiceTests
+    {
+    }
+}
