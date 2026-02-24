68c699a1be205c2d639704d6eba7a4541ed0360b
TheSharpKillers <thomasbn2409@gmail.com>
2026-02-24 15:03:46 +0100

    Started the ReportDownloadService class

--- PDFDownloader.Core/Services/ReportDownloadService.cs
+++ PDFDownloader.Core/Services/ReportDownloadService.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using PDFDownloader.Core.Interfaces;
+using PDFDownloader.Core.Models;
 
 namespace PDFDownloader.Core.Services
 {
-    internal class ReportDownloadService
+    public class ReportDownloadService
     {
+        private readonly IMetadataReader _metadataReader;
+        private readonly IReportDownloader _reportDownloader;
+        private readonly IResultWriter _resultWriter;
+
+        public ReportDownloadService(IMetadataReader metadataReader, IReportDownloader reportDownloader, IResultWriter resultWriter)
+        {
+            _metadataReader = metadataReader;
+            _reportDownloader = reportDownloader;
+            _resultWriter = resultWriter;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            // Read metadata
+
+            // Loop through each report
+            
+            // Write results
+        }
     }
 }
