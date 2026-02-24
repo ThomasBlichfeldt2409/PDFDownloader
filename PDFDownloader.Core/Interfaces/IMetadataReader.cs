2387e46a47f968442d727110678820ee13743d9c
TheSharpKillers <thomasbn2409@gmail.com>
2026-02-24 19:25:58 +0100

    Update IMetadataReader.cs

--- PDFDownloader.Core/Interfaces/IMetadataReader.cs
+++ PDFDownloader.Core/Interfaces/IMetadataReader.cs
@@ -4,6 +4,7 @@ namespace PDFDownloader.Core.Interfaces
 {
     public interface IMetadataReader
     {
+        // Uses Task to make ReadAsync future-proof, async await can be used for this method
         Task<List<ReportMetadata>> ReadAsync();
     }
 }
